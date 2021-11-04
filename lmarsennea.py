ab432b702a8658742c54df7e92597737dcf2ac07
Tomasz Gągor <tgagor@gmail.com>
2021-11-05 00:41:42 +0100

    Best I can get for now

--- lmarsennea.py
+++ lmarsennea.py
@@ -4,18 +4,16 @@ import random
 from multiprocessing import Pool, cpu_count
 # from multiprocessing.pool import ThreadPool
 from functools import cache, lru_cache
+import cProfile
+from sympy.ntheory.primetest import mr
 
-Sbuf = {}
 
-
-# @lru_cache(maxsize=100)
-@cache
+# it's counting only forward and require only one previous value actually
+@lru_cache(maxsize=10)
 def S(k, N):
-    # return 4 if k == 1 else S(k-1)**2-2
     return 4 if k == 1 else (S(k-1, N)**2-2) % N
 
 
-@cache
 def test_lucasa_lehmera(p):
     """
     Testujemy czy 2^p-1 jest pierwsza
@@ -26,8 +24,7 @@ def test_lucasa_lehmera(p):
     # martwe sprawdzenie - przy obecnych zalozeniach nigdy nie wypadnie
     N = 2**p-1
     for k in range(1, p):
-        # if S(k) % N == 0:
-        if S(k, N) % N == 0:
+        if S(k, N) == 0:
             return True
     return False
 
@@ -60,18 +57,33 @@ def test_rabina(n):
     return True
 
 
-def check_if_prime(exponent):
-    # LM wymagaja pierwszego p
-    if test_rabina(exponent):
-        # if test_rabina(2**i-1): # sprawdzamy czy nie bedzie szybciej :)
-        if(test_lucasa_lehmera(exponent)):
-            res = "LM: 2^%d-1 = %d" % (exponent, 2**exponent-1)
-            print(res)
+def check_if_prime(p):
+    # LM postaci 2**p-1 mogą być pierwsze tylko gdy p jest pierwsze
+    # co nie jest jednak warunkiem dostatecznym
+
+    # alternative to my test
+    # https://www.johndcook.com/blog/2019/02/25/prime-test/
+    # bases = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    # if mr(p, bases):
+
+    # https://bigprimes.org/
+
+    if test_rabina(p):
+        # if test_rabina(2**p-1):  # sprawdzamy czy nie bedzie szybciej :)
+        # PS: nie jest :)
+        if(test_lucasa_lehmera(p)):
+            N = 2**p-1
+            print(f"LM: 2^{p}-1 = {N}")
 
 
-if __name__ == '__main__':
-    MAX_EXPONENT = 5000
+def main():
+    MAX_EXPONENT = 10000
     exponents = range(3, MAX_EXPONENT, 2)
 
     with Pool(cpu_count()) as p:
         p.map(check_if_prime, exponents)
+
+
+if __name__ == '__main__':
+    # cProfile.run('main()')
+    main()
